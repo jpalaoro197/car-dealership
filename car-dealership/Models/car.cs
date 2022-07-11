b6dc41f37593ed55a5818a197472868ae65fb58a
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-11 10:24:07 -0700

    add getter methods to all private car fields.

--- car-dealership/Models/car.cs
+++ car-dealership/Models/car.cs
@@ -18,6 +18,15 @@ namespace Dealership.Models
     {
       return _makeModel;
     }
+    public int GetPrice()
+    {
+      return _price;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
+    }
 
     public bool WorthBuying( int maxPrice)
     {
